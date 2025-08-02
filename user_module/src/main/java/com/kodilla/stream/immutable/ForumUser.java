7204bee89258dd163e931b80c55463a65e2592b4
Patryk Rejent <82150867+Patryk244@users.noreply.github.com>
2025-08-02 12:48:49 +0200

    second commit

--- user_module/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ user_module/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,8 +1,8 @@
 package com.kodilla.stream.immutable;
 
 public final class ForumUser {
-    final String username;
-    final String realName;
+    private final String username;
+    private final String realName;
 
     public ForumUser(final String username, final String realName) {
         this.username = username;
